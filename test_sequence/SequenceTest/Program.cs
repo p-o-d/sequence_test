c2fdfd949729d1afcde4237fd03c2968bc391dab
Andrey Ivanenko <pcuzer@gmail.com>
2018-06-11 11:23:53 +0300

    init

---
+++ test_sequence/SequenceTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace SequenceTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var sequence = new TaskSequence())
+			{
+				for (int i = 1; i < 4; i++)
+				{
+					var index = i;
+					sequence.Enqueue(() =>
+					{
+						Thread.Sleep(index * 1000);
+						Console.WriteLine($"Task #{index} finished.");
+					});
+					Console.WriteLine($"Task #{index} added.");
+				}
+
+				Thread.Sleep(1200);
+
+				sequence.Enqueue(() =>
+				{
+					Thread.Sleep(1000);
+					throw new Exception("Task #4 failed.");
+				});
+				Console.WriteLine("Task #4 added.");
+
+				sequence.Enqueue(() =>
+				{
+					Thread.Sleep(500);
+					Console.WriteLine("Task #5 finished.");
+				});
+				Console.WriteLine("Task #5 added.");
+			}
+
+			Console.WriteLine("All tasks finished!");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ test_sequence/SequenceTest/TaskSequence.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace SequenceTest
+{
+	class TaskSequence : IDisposable
+	{
+		private readonly AutoResetEvent _signal = new AutoResetEvent(false);
+		private readonly Queue<Action> _taskQueue = new Queue<Action>();
+		private readonly Thread _worker;
+
+		public TaskSequence()
+		{
+			_worker = new Thread(Execute);
+			_worker.Start();
+		}
+
+		public void Dispose()
+		{
+			_taskQueue.Enqueue(null);
+			_signal.Set();
+			_worker.Join();
+			_signal.Close();
+		}
+
+		public void Enqueue(Action task)
+		{
+			if (task == null)
+				throw new ArgumentNullException(nameof(task));
+
+			lock (_taskQueue)
+			{
+				_taskQueue.Enqueue(task);
+				_signal.Set();
+			}
+		}
+
+		private void Execute()
+		{
+			while (true)
+			{
+				while (true)
+				{
+					Action task = null;
+					lock (_taskQueue)
+					{
+						if (_taskQueue.Any())
+							task = _taskQueue.Dequeue();
+						else
+							break;
+					}
+
+					if (task == null)
+						return;
+
+					try
+					{
+						task();
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(e.Message);
+					}
+				}
+
+				_signal.WaitOne();
+			}
+		}
+	}
+}
